d66d1500864ee56b84fb5149a59419a939ab3402
Chimdie <nzeyoungwealth@gmail.com>
2024-02-09 09:46:26 +0100

    feat: email subscription schema

---
+++ app/schema/subscribe.schema.ts
@@ -0,0 +1,7 @@
+import * as yup from "yup";
+
+export const SubscribeSchema = yup.object().shape({
+  email: yup.string().email("Invalid email address").required("Email is required"),
+});
+
+export type SubscribeSchema = yup.InferType<typeof SubscribeSchema>;
