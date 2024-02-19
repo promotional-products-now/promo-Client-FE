32b6136be9284704e926bc84698a36e03137f509
Hart102 <hartongit102@gmail.com>
2024-02-19 12:58:09 -0500

    Designed the Our Guarantees pages

---
+++ app/contents/guarantees.ts
@@ -0,0 +1,32 @@
+export const guarantees = [
+  {
+    id: 0,
+    title: "ROCK SOLID GUARANTEE #1",
+    subTitle: "100% Product Satisfaction Guarantee",
+    body: "Here is a 5 Rock Solid Guarantees to give you peace of mind and confidence that yoursatisfaction is our number 1 priority at all times.",
+  },
+  {
+    id: 0,
+    title: "ROCK SOLID GUARANTEE #1",
+    subTitle: "100% Product Satisfaction Guarantee",
+    body: "Here is a 5 Rock Solid Guarantees to give you peace of mind and confidence that yoursatisfaction is our number 1 priority at all times.",
+  },
+  {
+    id: 0,
+    title: "ROCK SOLID GUARANTEE #1",
+    subTitle: "100% Product Satisfaction Guarantee",
+    body: "Here is a 5 Rock Solid Guarantees to give you peace of mind and confidence that yoursatisfaction is our number 1 priority at all times.",
+  },
+  {
+    id: 0,
+    title: "ROCK SOLID GUARANTEE #1",
+    subTitle: "100% Product Satisfaction Guarantee",
+    body: "Here is a 5 Rock Solid Guarantees to give you peace of mind and confidence that yoursatisfaction is our number 1 priority at all times.",
+  },
+  {
+    id: 0,
+    title: "ROCK SOLID GUARANTEE #1",
+    subTitle: "100% Product Satisfaction Guarantee",
+    body: "Here is a 5 Rock Solid Guarantees to give you peace of mind and confidence that yoursatisfaction is our number 1 priority at all times.",
+  },
+];
