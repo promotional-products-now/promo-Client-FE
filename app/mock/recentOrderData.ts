2bd7899ed568adfc97cc7d93bccba95ff7f801bd
sammy-code98 <sammysmart98@yahoo.com>
2024-02-21 21:15:03 +0100

    feat: user profile/account details - user profile ui

---
+++ app/mock/recentOrderData.ts
@@ -0,0 +1,23 @@
+export const recentOrders = [
+  {
+    id: 1,
+    title: "Mix N Match Type-C Flash Drive",
+    productImg: "https://nextui-docs-v2.vercel.app/images/hero-card-complete.jpeg",
+    quantity: "50",
+    price: "$238.00",
+  },
+  {
+    id: 2,
+    title: "Mix N Match Type-C Flash Drive",
+    productImg: "https://nextui-docs-v2.vercel.app/images/hero-card-complete.jpeg",
+    quantity: "50",
+    price: "$238.00",
+  },
+  {
+    id: 3,
+    title: "Mix N Match Type-C Flash Drive",
+    productImg: "https://nextui-docs-v2.vercel.app/images/hero-card-complete.jpeg",
+    quantity: "50",
+    price: "$238.00",
+  },
+];
