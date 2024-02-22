f41687f8a4af08759fc27afa61229a1c4a56f5a4
sammy-code98 <sammysmart98@yahoo.com>
2024-02-22 18:57:54 +0100

    fix:resolve all issues raised by maintainer

--- app/components/Card/RecentOrderCard.tsx
+++ app/components/Card/RecentOrderCard.tsx
@@ -1,49 +1,43 @@
-import { Image, Button } from "@nextui-org/react";
+import { Image, Button, Divider } from "@nextui-org/react";
 import { GrCart } from "react-icons/gr";
 
 type RecentOrderCardProps = {
-  id: number,
-  title: string,
-  productImg: string,
-  price: string,
-  quantity: string
-}
+  id: number;
+  title: string;
+  productImg: string;
+  price: string;
+  quantity: string;
+};
 
 export default function RecentOrderCard(props: RecentOrderCardProps) {
   return (
-    <div className='border  border-lightGray p-6 flex flex-wrap justify-between items-center gap-3 mb-4'>
-      <div className='flex gap-4 items-center'>
-        <Image
-          className='rounded-none'
-          width={150}
-          alt='product image'
-          src={props.productImg}
-        />
+    <div className="border  border-lightGray p-6 flex flex-wrap justify-between items-center gap-3 mb-4">
+      <div className="flex gap-4 items-center">
+        <Image className="rounded-none" width={150} alt="product image" src={props.productImg} />
 
         <div>
-          <p className='text-gray text-base'>{props.title}</p>
+          <p className="text-gray text-base">{props.title}</p>
 
-          <p className='text-dark text-base font-medium'>Quantity : {props.quantity}</p>
+          <p className="text-dark text-base font-medium">Quantity : {props.quantity}</p>
         </div>
       </div>
-
-      <div className='bg-lightGray w-px  h-16'></div>
+      <Divider orientation="vertical" className="bg-lightGray w-px  h-16" />
       <div>
-        <p className='text-gray text-base'>Price</p>
-        <p className='text-primary font-medium'>{props.price}</p>
+        <p className="text-gray text-base">Price</p>
+        <p className="text-primary font-medium">{props.price}</p>
       </div>
-
-      <div className='bg-lightGray w-px  h-16'></div>
-
+      <Divider orientation="vertical" className="bg-lightGray w-px  h-16" />
       <div>
         <Button
-          variant='solid'
-          color='primary'
-          radius='none'
-          className='text-lg'
-          startContent={<GrCart className='text-white text-xl' />}>View Product</Button>
+          variant="solid"
+          color="primary"
+          radius="none"
+          className="text-lg"
+          startContent={<GrCart className="text-white text-xl" />}
+        >
+          View Product
+        </Button>
       </div>
-
     </div>
-  )
+  );
 }
--- app/routes/Details/route.tsx
+++ app/routes/Details/route.tsx
@@ -1,66 +1,57 @@
-import { Link } from '@remix-run/react';
+import { Link } from "@remix-run/react";
 import { MdOutlineMailOutline } from "react-icons/md";
 import { LuEye, LuMapPin } from "react-icons/lu";
-import RecentOrderCard from 'app/components/Card/RecentOrderCard';
-import { recentOrders } from 'app/mock/recentOrderData';
-
-
+import RecentOrderCard from "app/components/Card/RecentOrderCard";
+import { recentOrders } from "app/mock/recentOrderData";
 
 export default function Detatils(): JSX.Element {
   return (
     <>
-      <div className='py-4 md:py-8'>
-        <h1 className='text-2xl md:text-3xl text-dark  text-center space-y-8'>Account Details</h1>
-
-
-        <div className='grid  grid-cols-1  md:grid-cols-3  gap-4 py-4 px-2 md:px-6 w-full md:w-5/6 mx-auto'>
-          <div className='space-y-4'>
-            <h1 className='text-dark text-lg md:text-xl font-medium'>Alex Martenis</h1>
+      <div className="py-4 md:py-8">
+        <h1 className="text-2xl md:text-3xl text-dark  text-center space-y-8">Account Details</h1>
+        <div className="grid  grid-cols-1  md:grid-cols-3  gap-4 py-4 px-2 md:px-6 w-full md:w-5/6 mx-auto">
+          <div className="space-y-4">
+            <h1 className="text-dark text-lg md:text-xl font-medium">Alex Martenis</h1>
 
             <div>
-              <div className='flex gap-3 py-3'>
-                <MdOutlineMailOutline className='text-primary text-xl' />
-                <div className='space-y-2'>
+              <div className="flex gap-3 py-3">
+                <MdOutlineMailOutline className="text-primary text-xl" />
+                <div className="space-y-2">
                   <p>alex@marteni@gmail.com</p>
-                  <Link to="#" className='text-base text-yellow'>Edit account info</Link>
+                  <Link to="#" className="text-base text-yellow">
+                    Edit account info
+                  </Link>
                 </div>
               </div>
 
-              <div className='flex  gap-3 py-3'>
-                <LuEye className='text-primary text-xl' />
-                <div className='space-y-2'>
+              <div className="flex  gap-3 py-3">
+                <LuEye className="text-primary text-xl" />
+                <div className="space-y-2">
                   <p>*********</p>
-                  <Link to="/change-password" className='text-base text-yellow'>Change Password</Link>
+                  <Link to="/change-password" className="text-base text-yellow">
+                    Change Password
+                  </Link>
                 </div>
               </div>
-              <div className='flex gap-3 py-3'>
-                <LuMapPin className='text-primary text-xl' />
-                <div className='space-y-2'>
+              <div className="flex gap-3 py-3">
+                <LuMapPin className="text-primary text-xl" />
+                <div className="space-y-2">
                   <p>Sydney, Australia</p>
                 </div>
-
               </div>
             </div>
-
           </div>
-          <div className='col-span-2'>
-            <h1 className='text-dark text-lg md:text-xlx'>Your Recent Orders</h1>
-            <hr className='text-lightGray mt-4  h-4 w-full' />
-
-
+          <div className="col-span-2">
+            <h1 className="text-dark text-lg md:text-xl">Your Recent Orders</h1>
+            <hr className="text-lightGray mt-4  h-4 w-full" />
             <div>
               {recentOrders.map((orders) => (
-                <RecentOrderCard
-                  {...orders}
-                />
-
+                <RecentOrderCard {...orders} />
               ))}
             </div>
           </div>
-
         </div>
       </div>
-
     </>
-  )
+  );
 }
