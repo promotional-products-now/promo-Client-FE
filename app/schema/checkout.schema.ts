a997768a625daa0fadde441d71d07277e307c846
wisdom nwakaku <nwakakucc@gmail.com>
2024-02-25 08:56:13 +0100

    checkout

--- app/schema/checkout.schema.ts
+++ app/schema/checkout.schema.ts
@@ -1,15 +1,19 @@
 import * as yup from "yup";
 
+
 export const CheckoutSchema = yup.object().shape({
   firstName: yup.string().required(),
   lastName: yup.string().required(),
   companyName: yup.string().required(),
   apartment: yup.string().required(),
   address: yup.string().required(),
-  subarb: yup.string().required(),
+  suburb: yup.string().required(),
+  state: yup.string().required(),
   postalCode: yup.number().required().positive().integer(),
+  country: yup.string().required(),
   delivery: yup.string().required(),
   email: yup.string().email("Invalid email address").required("Email is required"),
+  image: yup.string(),
 });
 
 export type CheckoutSchema = yup.InferType<typeof CheckoutSchema>;
