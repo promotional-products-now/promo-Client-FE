247a0b517c77b2782c8199f2c3e7f6b86d2d1f25
Chimdie <nzeyoungwealth@gmail.com>
2024-02-28 19:08:34 +0100

    feat: search dropdown field

---
+++ app/components/Header/SearchDropdown.tsx
@@ -0,0 +1,137 @@
+import { useState } from "react";
+import {
+  Button,
+  Card,
+  CardBody,
+  Input,
+  Checkbox,
+  Selection,
+  Select,
+  SelectItem,
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@nextui-org/react";
+import { IoIosArrowDown } from "react-icons/io";
+
+const colors = [
+  "#8A2C51",
+  "#DD463D",
+  "#DF70AA",
+  "#ED965E",
+  "#FBCC62",
+  "#84BA5A",
+  "#C4E3FF",
+  "#586CB3",
+  "#2B417A",
+  "#8160A5",
+  "#FFFFFF",
+  "#8B5D50",
+  "#99979A",
+  "#18181A",
+  "#E5E5E7",
+  "",
+];
+const sortFilter = [
+  { label: "Price", value: "price" },
+  { label: "Lowest", value: "lowest" },
+];
+export const SearchDropdown = () => {
+  const [values, setValues] = useState<Selection>(new Set(["price", "lowest"]));
+
+  return (
+    <Popover placement="bottom" backdrop="transparent">
+      <PopoverTrigger>
+        <Button isIconOnly variant="faded" className="border-none">
+          <IoIosArrowDown />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="p-1">
+        <Card shadow="none" radius="none" className=" border-none bg-transparent">
+          <CardBody className="p-3">
+            <div className="flex flex-col md:flex-row space-x-4">
+              <div className="space-y-4 p-4">
+                <div className="space-y-1">
+                  <Input
+                    type="text"
+                    radius="none"
+                    label="Quantity"
+                    labelPlacement="outside"
+                    placeholder="500"
+                    className="text-primary max-w"
+                  />
+                  <Checkbox size="sm">In stock now</Checkbox>
+                </div>
+                <div className="grid grid-cols-1 md:grid-cols-2 space-x-3">
+                  <div className="space-y-1">
+                    <Input
+                      type="text"
+                      radius="none"
+                      label="Price from"
+                      labelPlacement="outside"
+                      placeholder="500"
+                      className="text-primary"
+                      startContent="$"
+                      endContent={<IoIosArrowDown />}
+                    />
+                    <span className="text-xs text-gray">Plus Branding cost and Freight cost</span>
+                  </div>
+                  <div className="space-y-1">
+                    <Input
+                      type="text"
+                      radius="none"
+                      label="Price To"
+                      labelPlacement="outside"
+                      placeholder="500"
+                      className="text-primary"
+                      startContent="$"
+                      endContent={<IoIosArrowDown />}
+                    />
+                    <></>
+                  </div>
+                </div>
+                <div className="flex w-full max-w-full flex-col gap-2">
+                  <Select
+                    label="Sort results by:"
+                    labelPlacement="outside"
+                    selectionMode="multiple"
+                    placeholder="Price, lowest first"
+                    selectedKeys={values}
+                    onSelectionChange={setValues}
+                  >
+                    {sortFilter.map((ss) => (
+                      <SelectItem key={ss.value} value={ss.value}>
+                        {ss.label}
+                      </SelectItem>
+                    ))}
+                  </Select>
+                </div>
+                <div className="flex justify-end">
+                  <Button type="submit" className="bg-primary text-white rounded-none px-4">
+                    Reset
+                  </Button>
+                </div>
+              </div>
+              <div className="space-y-4 p-4">
+                <h3 className="text-lg font-semibold">Search by Colours</h3>
+                <span className="text-sm text-gray font-normal">
+                  Select one or more colours to highlight products available
+                </span>
+                <div className="grid grid-cols-4 gap-3 bg-slate-100 p-4">
+                  {colors.map((c, i) => (
+                    <Button
+                      isIconOnly
+                      key={i}
+                      className="w-10 h-10 aspect-square rounded-full"
+                      style={{ backgroundColor: c }}
+                    />
+                  ))}
+                </div>
+              </div>
+            </div>
+          </CardBody>
+        </Card>
+      </PopoverContent>
+    </Popover>
+  );
+};
