d466724ad27cb530d8d69d18b206460450ab9eef
Chimdie <nzeyoungwealth@gmail.com>
2024-02-28 22:08:56 +0100

    fix: product card

--- app/components/Card/ProductCard.tsx
+++ app/components/Card/ProductCard.tsx
@@ -1,56 +1,80 @@
-import { Link } from "@remix-run/react";
+import { Card, CardHeader, CardBody, Image, Link, Button, useDisclosure } from "@nextui-org/react";
+import { useSetAtom } from "jotai";
 import { BsCart3 } from "react-icons/bs";
 import { FiEye } from "react-icons/fi";
-import { Card, CardHeader, CardBody, CardFooter, Divider, Image } from "@nextui-org/react";
-import { items } from "app/api_dummy";
+import { PreviewProduct } from "./PreviewProduct";
+import { productAtom } from "app/atoms/product.atom";
 
-interface ProductCardProps {
+export type ProductCardProps = {
   image: string;
   title: string;
   subtitle: string;
   price: string;
   newPrice: string;
   qunatity: string;
-  key: number
-}
+};
 
-const ProductCard = ({ image, title, subtitle, price, newPrice, qunatity, key }: ProductCardProps) => {
+const ProductCard = ({ image, title, subtitle, price, newPrice, qunatity }: ProductCardProps) => {
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+  const setProduct = useSetAtom(productAtom);
+
+  const props = { image, title, subtitle, price, newPrice, qunatity };
+
+  const handlePreviewProd = () => {
+    onOpen();
+    setProduct(props); // doesn't really work...yikes
+  };
+
   return (
-    <Card className="col-span-1 cursor-pointer group" radius="none" key={key}>
-      <CardHeader className="aspect-square w-full relative overflow-hidden p-0">
-        <Image
-          alt={title}
-          radius="none"
-          src={image}
-          removeWrapper
-          className="object-cover h-full w-full transition aspect-square inset-0"
-        />
+    <>
+      <Card className="col-span-1 cursor-pointer group" radius="none">
+        <CardHeader className="aspect-square w-full relative overflow-hidden p-0">
+          <Image
+            alt={title}
+            radius="none"
+            src={image}
+            removeWrapper
+            className="object-cover h-full w-full transition aspect-square inset-0"
+          />
 
-        <div className="flex flex-row absolute bottom-0 right-0 left-0 opacity-0 group-hover:opacity-100 transition w-full text-white z-20">
-          <div className="bg-orange px-2 py-2 flex flex-row items-center flex-1 gap-2 justify-center ">
-            <FiEye /> <div className="">Preview</div>
+          <div className="grid grid-cols-2 absolute bottom-0 opacity-0 group-hover:opacity-100 transition w-full z-20">
+            <Button
+              radius="none"
+              className="bg-orange text-white"
+              startContent={<FiEye />}
+              onPress={handlePreviewProd}
+            >
+              Preview
+            </Button>
+            <Button
+              as={Link}
+              href="#"
+              radius="none"
+              className="bg-primary text-white"
+              startContent={<BsCart3 />}
+            >
+              View
+            </Button>
           </div>
-          <div className="bg-primary px-2 py-2 flex flex-row items-center flex-1 gap-2 justify-center">
-            <BsCart3 /> <div className="">View product</div>
-          </div>
-        </div>
-      </CardHeader>
+        </CardHeader>
 
-      <CardBody className="overflow-visible p-2 gap-3">
-        <div className="text-primary capitalize font-semibold">{title}</div>
-        <div className="text-black">{subtitle}</div>
-        <div className="flex flex-row text-sm justify-between">
-          <div className="text-gray-700 flex flex-row gap-1">
-            <p>
-              {" "}
-              from <span className="text-orange">{price}</span> to
-            </p>
-            <span className="text-primary">{newPrice}</span>
+        <CardBody className="overflow-visible p-2 gap-3">
+          <div className="text-primary capitalize font-semibold text-sm">{title}</div>
+          <div className="text-black text-xs">{subtitle}</div>
+          <div className="flex text-sm justify-between">
+            <div className="text-gray-700 flex gap-1">
+              <p className="text-xs">
+                from <span className="text-orange text-xs">{price}</span> to
+              </p>
+              <span className="text-primary text-xs">{newPrice}</span>
+            </div>
+            <div className="text-xs">{qunatity}</div>
           </div>
-          <div className="">{qunatity}</div>
-        </div>
-      </CardBody>
-    </Card>
+        </CardBody>
+      </Card>
+
+      <PreviewProduct isOpen={isOpen} onOpenChange={onOpenChange} />
+    </>
   );
 };
 
