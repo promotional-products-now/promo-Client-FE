24d9e2e57bb57538fcca41c3826250f7810885f9
Chimdie <nzeyoungwealth@gmail.com>
2024-03-03 22:01:47 +0100

    feat: error boundary handler

--- app/root.tsx
+++ app/root.tsx
@@ -11,14 +11,13 @@ import {
 } from "@remix-run/react";
 import { cssBundleHref } from "@remix-run/css-bundle";
 import { NextUIProvider } from "@nextui-org/react";
+import { isRouteErrorResponse, useRouteError } from "@remix-run/react";
 import { Header } from "app/components/Header";
 import { Footer } from "app/components/Footer";
 import stylesheet from "./tailwind.css";
 
 export const links: LinksFunction = () => [
-  ...(cssBundleHref
-    ? [{ rel: "stylesheet", href: cssBundleHref }]
-    : []),
+  ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
   { rel: "stylesheet", href: stylesheet },
 ];
 
@@ -61,4 +60,30 @@ export default function App() {
 
 export async function loader() {
   return json({ ENV: { SALES_CONTACT: process.env.SALES_CONTACT } });
+}
+
+export function ErrorBoundary() {
+  const error = useRouteError();
+
+  if (isRouteErrorResponse(error)) {
+    return (
+      <div className="flex flex-col justify-between h-screen bg-red-400">
+        <h1>
+          {error.status} {error.statusText}
+        </h1>
+        <p>{error.data}</p>
+      </div>
+    );
+  } else if (error instanceof Error) {
+    return (
+      <div className="flex flex-col justify-between h-screen bg-red-400">
+        <h1>Error</h1>
+        <p>{error.message}</p>
+        <p>The stack trace is:</p>
+        <pre>{error.stack}</pre>
+      </div>
+    );
+  } else {
+    return <h1>Unknown Error</h1>;
+  }
 }
