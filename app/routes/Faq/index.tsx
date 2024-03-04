249e798bbad45b23f0e07a01c68900b9384df360
Hart102 <hartongit102@gmail.com>
2024-03-04 11:05:55 -0500

    made the FAQs text bigger

--- app/routes/Faq/index.tsx
+++ app/routes/Faq/index.tsx
@@ -1,11 +1,12 @@
-import QuestionAccordion from "app/components/QuestionAccordion";
-import { questions } from "app/components/QuestionAccordion/data";
+import { Accordion, AccordionItem } from "@nextui-org/react";
+import { FaQuestion } from "react-icons/fa";
+import { questions } from "app/contents/faqs";
 
 const Faq = () => {
   return (
     <div className="flex flex-col gap-3 w-full mx-auto py-10 lg:px-5 lg:w-[80%]">
       <div className="flex flex-col gap-3 text-center">
-        <b className="text-lg">FAQs</b>
+        <h2 className="text-3xl font-extrabold">FAQs</h2>
         <p>
           Here are the most frequenty asked questions. We are here to help you, so please feel free
           to{" "}
@@ -13,13 +14,31 @@ const Faq = () => {
             Contact us
           </span>
         </p>
-        <b className="text-left text-yellow-400">FAQ-Your Artwork and Logo</b>
       </div>
 
-      {/*=== Ordering Promotional Products ===*/}
-      <div className="mt-5">
-        <b className="text-left text-yellow-400">Ordering Promotional Products</b>
-        <QuestionAccordion questions={questions} />
+      <div className="flex flex-col gap-2 mt-5">
+        <b className="text-left text-yellow">FAQ-Your Artwork and Logo</b>
+        <Accordion showDivider={false}>
+          {questions &&
+            questions.map((question) => (
+              <AccordionItem
+                key={question.id}
+                aria-label={`Accordion ${question.id}`}
+                startContent={
+                  <div className="border border-blue-300 p-1 rounded">
+                    <FaQuestion color="blue" size={10} />
+                  </div>
+                }
+                title={question.title}
+                className="border border-neutral-200 my-2 px-3"
+              >
+                <div className="border-t border-neutral-200 mb-2"></div>
+                <div className="w-full px-3">
+                  <p className="text-foreground-600 text-sm leading-loose">{question.body}</p>
+                </div>
+              </AccordionItem>
+            ))}
+        </Accordion>
       </div>
     </div>
   );
