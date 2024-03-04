fc3452ecdb81a306311c65f0b92831c5553eb716
Hart102 <hartongit102@gmail.com>
2024-03-04 13:15:29 -0500

    increased the blog title

--- app/routes/Blog/index.tsx
+++ app/routes/Blog/index.tsx
@@ -6,12 +6,12 @@ import { blogPosts } from "app/mock/blogData";
 
 const Blog = () => {
   return (
-    <div className="flex flex-col gap-10 w-full mx-auto py-10 text-sm lg:w-[90%]">
+    <div className="flex flex-col gap-10 w-full mx-auto py-10 px-5 lg:px-0 lg:w-[80%]">
       <div className="flex flex-col gap-1 text-center ">
         <h2 className="text-4xl font-extrabold">Our Blog</h2>
-        <p className="text-gray text-2xl">Browse our latest news</p>
+        <h3 className="text-gray text-2xl">Browse our latest news</h3>
       </div>
-      <div className="grid grid-cols-1 gap-14 sm:grid-cols-2 md:grid-cols-3">
+      <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 md:grid-cols-3">
         {blogPosts &&
           blogPosts.map((post) => (
             <Card key={post.id} isPressable className="rounded-none shadow-none">
@@ -25,8 +25,8 @@ const Blog = () => {
                   src={image}
                 />
               </CardBody>
-              <CardFooter className="text-small flex flex-col gap-2 justify-between text-left px-3">
-                <b className="line-clamp-2">{post.title}</b>
+              <CardFooter className="bg-white-bg flex flex-col gap-2 justify-between text-left px-3">
+                <h4 className="line-clamp-2 text-[18px]">{post.title}</h4>
                 <p className="text-default-500 line-clamp-2 ">{post.body}</p>
                 <div className="w-full">
                   <Link
