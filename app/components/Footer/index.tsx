1e5c0429cf7908e9e90ad97e1a1ce7bccff13cd7
Chimdie <nzeyoungwealth@gmail.com>
2024-03-04 15:19:08 +0100

    fix: footer alignment - fixes #40

--- app/components/Footer/index.tsx
+++ app/components/Footer/index.tsx
@@ -6,7 +6,7 @@ import { FiPhoneCall } from "react-icons/fi";
 import logo from "app/assets/logo.svg";
 import { SubscribeSchema } from "app/schema/subscribe.schema";
 import { FooterLinkComp } from "./FooterLinkComp";
-import { aboutLinks, categoryLinks, companyInfo, faqLinks, otherLinks } from "./links";
+import { aboutLinks, categoryLinks, companyInfo, faqLinks, otherLinks, socialLinks } from "./links";
 
 export function Footer() {
   const {
@@ -25,36 +25,35 @@ export function Footer() {
 
   return (
     <footer className="bg-white dark:bg-gray-900">
-      <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
+      <hr className="border-neutral-200 sm:mx-auto dark:border-gray-700" />
+
+      <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8 space-y-6 md:space-y-10">
         <div className="sm:flex sm:items-center sm:justify-between space-y-6 sm:space-y-0 w-full">
-          <h1 className="text-4xl text-black font-semibold">Newsletter</h1>
+          <h1 className="text-lg md:text-3xl text-black font-semibold">Newsletter</h1>
           <div className="flex sm:justify-end w-full">
-            <form onSubmit={handleSubmit(onSubmit)} className="w-full sm:w-2/4">
-              <div className="flex items-center w-full">
+            <form onSubmit={handleSubmit(onSubmit)} className="w-full sm:w-3/4">
+              <div className="flex items-center w-full border-">
                 <Input
                   size="sm"
+                  radius="none"
                   placeholder="Your email address"
-                  startContent={<CiMail className="text-5xl" />}
+                  startContent={<CiMail className="text-xl" />}
                   className="rounded-s-2xl rounded-e-none bg-transparent w-full overflow-hidden"
                   {...register("email")}
                   isInvalid={!!errors.email}
-                  errorMessage={errors.email && errors.email.message}
-                  endContent={
-                    <Button
-                      type="submit"
-                      size="lg"
-                      variant="ghost"
-                      className="border-none rounded-s-none bg-primary text-white"
-                    >
-                      Subscribe
-                    </Button>
-                  }
                 />
+                <Button
+                  type="submit"
+                  size="lg"
+                  variant="ghost"
+                  className="border-none rounded-s-none bg-primary text-white"
+                >
+                  Subscribe
+                </Button>
               </div>
             </form>
           </div>
         </div>
-        <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
 
         <>
           <div className="grid gap-8 sm:gap-6 sm:grid-cols-4">
@@ -72,8 +71,18 @@ export function Footer() {
                   </div>
                 ))}
               </div>
-              <div>
+              <div className="space-y-4">
                 <h2 className="text-xl text-black font-semibold">Follow Us</h2>
+                <div className="flex items-center space-x-3">
+                  {socialLinks.map((link) => {
+                    const IconTag = link.icon;
+                    return (
+                      <Link href={link.pathname} className="">
+                        <IconTag className="text-primary text-2xl" />
+                      </Link>
+                    );
+                  })}
+                </div>
               </div>
               <p className="text-sm font-normal text-gray">
                 Copyright 2023 Promotional Products Now
