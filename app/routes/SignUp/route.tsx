b92c56f58fce85e74ba7663e11cdc481cc1e93ac
sammy-code98 <sammysmart98@yahoo.com>
2024-03-05 12:40:26 +0100

    fix: resolve issues on create account page - make input label font bold - add global style for input underlines

--- app/routes/SignUp/route.tsx
+++ app/routes/SignUp/route.tsx
@@ -21,11 +21,11 @@ export default function SignUp(): JSX.Element {
   return (
     <>
       <div className="py-8">
-        <div className="space-y-4">
-          <h1 className="text-2xl md:text-4xl text-dark font-bold text-center">
+        <div className="space-y-4 px-2">
+          <h1 className="text-xl md:text-4xl text-dark font-bold text-center">
             Welcome to Promotional Products Now
           </h1>
-          <p className="text-lg md:text-xl text-dark text-center">
+          <p className="text-base md:text-xl text-dark text-center">
             Create your account for a smoother website and purchasing experience
           </p>
           <p className="text-center text-dark text-sm md:text-base">
@@ -38,12 +38,12 @@ export default function SignUp(): JSX.Element {
 
         <Form
           method="post"
-          className="w-full  overflow-hidden py-4 md:py-12 flex flex-col items-center justify-center"
+          className="w-full  overflow-hidden py-4 px-4 md:py-12 flex flex-col items-center justify-center"
         >
           <div className="w-full md:w-4/5 xl:w-3/5 2xl:w-2/4  overflow-hidden py-4 md:py-12">
             <div className="flex flex-col sm:flex-row  gap-4 py-2">
               <div className="w-full">
-                <p className="text-base md:text-lg text-dark">FIRST NAME</p>
+                <p className="text-base font-bold text-dark">FIRST NAME</p>
                 <Input
                   type="text"
                   variant="underlined"
@@ -56,7 +56,7 @@ export default function SignUp(): JSX.Element {
                 />
               </div>
               <div className="w-full">
-                <p className="text-base md:text-lg text-dark">LAST NAME</p>
+                <p className="text-base font-bold text-dark">LAST NAME</p>
                 <Input
                   type="text"
                   variant="underlined"
@@ -72,7 +72,7 @@ export default function SignUp(): JSX.Element {
 
             <div className="flex flex-col sm:flex-row gap-4 py-2">
               <div className="w-full">
-                <p className="text-base md:text-lg text-dark">EMAIL ADDRESS</p>
+                <p className="text-base font-bold text-dark">EMAIL ADDRESS</p>
                 <Input
                   type="email"
                   variant="underlined"
@@ -85,7 +85,7 @@ export default function SignUp(): JSX.Element {
                 />
               </div>
               <div className="w-full">
-                <p className="text-base md:text-lg text-dark">PHONE</p>
+                <p className="text-base font-bold text-dark">PHONE</p>
                 <Input
                   type="text"
                   variant="underlined"
@@ -101,7 +101,7 @@ export default function SignUp(): JSX.Element {
 
             <div className="flex flex-col sm:flex-row gap-4 py-2">
               <div className="w-full">
-                <p className="text-base md:text-lg text-dark">PASSWORD</p>
+                <p className="text-base font-bold text-dark">PASSWORD</p>
                 <Input
                   type="password"
                   variant="underlined"
@@ -114,7 +114,7 @@ export default function SignUp(): JSX.Element {
                 />
               </div>
               <div className="w-full">
-                <p className="text-base  md:text-lg text-dark">REPEAT PASSWORD</p>
+                <p className="text-base  font-bold text-dark">REPEAT PASSWORD</p>
                 <Input
                   type="password"
                   variant="underlined"
@@ -129,7 +129,7 @@ export default function SignUp(): JSX.Element {
             </div>
 
             <div className="w-full">
-              <p className="text-base md:text-lg text-dark">ADDRESS 1</p>
+              <p className="text-base font-bold text-dark">ADDRESS 1</p>
               <Input
                 type="text"
                 variant="underlined"
@@ -143,7 +143,7 @@ export default function SignUp(): JSX.Element {
             </div>
 
             <div className="w-full">
-              <p className="text-base md:text-lg text-dark">ADDRESS 2</p>
+              <p className="text-base font-bold text-dark">ADDRESS 2</p>
               <Input
                 type="text"
                 variant="underlined"
@@ -158,7 +158,7 @@ export default function SignUp(): JSX.Element {
 
             <div className="flex flex-col sm:flex-row gap-4 py-2 w-full">
               <div className="w-full">
-                <p className="text-base md:text-lg text-dark">CITY</p>{" "}
+                <p className="text-base font-bold text-dark">CITY</p>{" "}
                 <Select
                   aria-label="city"
                   variant="underlined"
@@ -176,7 +176,7 @@ export default function SignUp(): JSX.Element {
                 </Select>
               </div>
               <div className="w-full">
-                <p className="text-base md:text-lg text-dark">STATE</p>
+                <p className="text-base font-bold text-dark">STATE</p>
                 <Select
                   aria-label="state"
                   variant="underlined"
@@ -196,7 +196,7 @@ export default function SignUp(): JSX.Element {
             </div>
 
             <div className="py-2 ">
-              <p className="text-base md:text-lg text-dark">POST CODE</p>
+              <p className="text-base font-bold text-dark">POST CODE</p>
               <Input
                 type="text"
                 variant="underlined"
