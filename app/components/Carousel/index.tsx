3d6c5dfd980cef7b5f3dcf2da87c05bf93a792e4
Chimdie <nzeyoungwealth@gmail.com>
2024-04-09 15:21:01 +0100

    fix: carousel alignment

--- app/components/Carousel/index.tsx
+++ app/components/Carousel/index.tsx
@@ -1,34 +1,47 @@
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Pagination } from "swiper/modules";
+import { A11y, Parallax, Navigation } from "swiper/modules";
 
-export default function Carousel({ children }: { children: React.ReactNode[] }) {
+export default function Carousel({
+  children,
+  numberOfItems,
+}: {
+  children: React.ReactNode[];
+  numberOfItems?: number;
+}) {
   return (
     <>
       <Swiper
-        // freeMode={true}
-        navigation={true}
+        navigation
+        parallax={true}
         pagination={{
           clickable: true,
         }}
         slidesPerView={1}
         spaceBetween={20}
         breakpoints={{
-          640: {
+          320: {
             slidesPerView: 2,
             spaceBetween: 20,
           },
+          640: {
+            slidesPerView: 2,
+            // spaceBetween: 20,
+          },
           768: {
-            slidesPerView: 3,
-            spaceBetween: 40,
+            slidesPerView: numberOfItems || 3,
+            // spaceBetween: 40,
+          },
+          1200: {
+            slidesPerView: numberOfItems || 4,
+            // spaceBetween: 40,
           },
           3024: {
-            slidesPerView: 4,
-            spaceBetween: 50,
+            slidesPerView: numberOfItems || 4,
+            // spaceBetween: 50,
           },
         }}
-        modules={[Navigation, Pagination]}
-        className="mySwiper"
+        modules={[Navigation, Parallax, A11y]}
       >
         {children &&
           children.map((node) => {
