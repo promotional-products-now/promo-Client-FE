8e821a3bab1ad7bab84eef5182b1994b597aabc6
Hart102 <hartongit102@gmail.com>
2024-07-20 00:11:37 -0400

    Added schema.org to this page

--- app/routes/Guarantees/index.tsx
+++ app/routes/Guarantees/index.tsx
@@ -3,7 +3,11 @@ import { guarantees } from "app/contents/guarantees";
 
 const Guarantees = () => {
   return (
-    <div className="flex flex-col gap-10 w-full p-4 lg:p-0 mx-auto md:w-4/5">
+    <div
+      itemScope
+      itemType="https://schema.org/OurGuarantees"
+      className="flex flex-col gap-10 w-full p-4 lg:p-0 mx-auto md:w-4/5"
+    >
       <div className="flex flex-col gap-2 text-center pt-10 md:pt-0">
         <h2 className="text-2xl md:text-3xl font-bold">Our Guarantees</h2>
         <p className="text-gray text-xl">
@@ -12,16 +16,30 @@ const Guarantees = () => {
         </p>
       </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
-        {guarantees.map((guarantee) => (
+      <div
+        itemScope
+        itemType="https://schema.org/guarantees"
+        className="grid grid-cols-1 md:grid-cols-2 gap-2"
+      >
+        {guarantees.map((guarantee, index) => (
           <div
             key={guarantee.id}
+            itemProp="itemListElement"
+            itemScope
+            itemType="https://schema.org/guarantee"
             className="py-6 px-4 flex flex-col md:flex-row justify-center items-center md:justify-between gap-4 border border-neutral-100"
           >
-            <div className="space-y-4 text-center md:text-start">
-              <h3 className="text-lg font-bold text-primary">{guarantee.title}</h3>
-              <p className="text-base font-medium text-yellow">{guarantee.description}</p>
-              <p className="text-base text-gray font-medium">{guarantee.body}</p>
+            <meta itemProp="position" content={`${index + 1}`} />
+            <div itemProp="item" className="space-y-4 text-center md:text-start">
+              <h3 itemProp="name" className="text-lg font-bold text-primary">
+                {guarantee.title}
+              </h3>
+              <p itemProp="description" className="text-base font-medium text-yellow">
+                {guarantee.description}
+              </p>
+              <p itemProp="description" className="text-base text-gray font-medium">
+                {guarantee.body}
+              </p>
             </div>
             <div className="order-first md:order-last">
               <GuaranteeIcon />
