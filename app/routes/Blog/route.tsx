1599e8e5c08c9793af1bdea22dbc76055d9daa49
Hart102 <hartongit102@gmail.com>
2024-07-19 23:47:54 -0400

    Added schema.org to this page

--- app/routes/Blog/route.tsx
+++ app/routes/Blog/route.tsx
@@ -50,23 +50,28 @@ const Blog = () => {
         <h2 className="text-2xl md:text-3xl font-bold">Our Blog</h2>
         <h3 className="text-default-500">Browse our latest news</h3>
       </div>
-      <div className="grid grid-cols-1 gap-y-8 gap-x-5 sm:grid-cols-2 md:grid-cols-3">
+      <div
+        itemScope
+        itemType="https://schema.org/Blog"
+        className="grid grid-cols-1 gap-y-8 gap-x-5 sm:grid-cols-2 md:grid-cols-3"
+      >
         {loaderData &&
           loaderData.posts &&
           loaderData.posts.length > 0 &&
           loaderData?.posts?.map((post: BlogCardProps) => (
-            <BlogCard
-              key={post?._id}
-              title={post?.title}
-              description={post?.description}
-              imageSrc={
-                post.imageSrc ??
-                "https://dfstudio-d420.kxcdn.com/wordpress/wp-content/uploads/2019/06/digital_camera_photo-1080x675.jpg"
-              }
-              _id={post?._id}
-              category={post?.category}
-              body={post?.body}
-            />
+            <div itemScope itemType="https://schema.org/BlogPosting" key={post?._id}>
+              <BlogCard
+                title={post?.title}
+                description={post?.description}
+                imageSrc={
+                  post.imageSrc ??
+                  "https://dfstudio-d420.kxcdn.com/wordpress/wp-content/uploads/2019/06/digital_camera_photo-1080x675.jpg"
+                }
+                _id={post?._id}
+                category={post?.category}
+                body={post?.body}
+              />
+            </div>
           ))}
       </div>
       <div className="flex items-center justify-center">
