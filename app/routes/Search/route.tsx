54ead33fd6492841b5466642506a5e86953858df
Chimdie <nzeyoungwealth@gmail.com>
2024-08-19 13:54:56 +0100

    fix: pagination

--- app/routes/Search/route.tsx
+++ app/routes/Search/route.tsx
@@ -1,7 +1,7 @@
 import { useDisclosure } from "@nextui-org/react";
 import { LoaderFunction } from "@remix-run/node";
-import { Link, useLoaderData, useLocation } from "@remix-run/react";
-import { SetStateAction, useCallback, useState } from "react";
+import { Link, useFetcher, useLoaderData, useLocation } from "@remix-run/react";
+import { SetStateAction, useCallback, useMemo, useState } from "react";
 import { useSetAtom } from "jotai";
 import { MdKeyboardDoubleArrowRight } from "react-icons/md";
 import { fetchProductsApi } from "app/api/products.api";
@@ -14,8 +14,10 @@ import TablePagination from "app/components/TablePagination";
 export const loader: LoaderFunction = async ({ request }: { request: { url: string } }) => {
   const url = new URL(request.url);
   const searchQuery = url.searchParams.get("q") || "";
+  const page = parseInt(url.searchParams.get("page") || "1", 1);
+  const limit = parseInt(url.searchParams.get("limit") || "10", 10);
 
-  const { data } = await fetchProductsApi({ search: searchQuery, page: 1, limit: 15 });
+  const { data } = await fetchProductsApi({ search: searchQuery, page, limit });
 
   return data;
 };
@@ -28,8 +30,7 @@ export default function SearchPage() {
   const setProductPrevData = useSetAtom(productPreviewAtom);
   const location = useLocation();
   const queryParam = new URLSearchParams(location.search).get("q");
-
-  console.log({ loaderData });
+  const fetcher = useFetcher();
 
   const searchResult = loaderData ? loaderData?.docs : [];
 
@@ -40,25 +41,30 @@ export default function SearchPage() {
 
   const handleNext = useCallback(
     (pageNumber: number) => {
-      if (searchResult.hasNext && !pageNumber) {
-        setCurrentPage(searchResult.nextPage);
+      if (loaderData?.hasNext && !pageNumber) {
+        setCurrentPage(loaderData?.nextPage);
       } else {
         setCurrentPage(pageNumber);
       }
+      fetcher.load(`/search?page=${pageNumber}&limit=${limit}`);
     },
-    [searchResult.hasNext, searchResult.nextPage],
+    [fetcher, limit, loaderData?.hasNext, loaderData?.nextPage],
   );
 
-  const handlePrevious = useCallback(() => {
-    if (searchResult.hasPrevious) {
-      setCurrentPage(searchResult.prevPage);
+  const handlePrevious = () => {
+    if (loaderData?.hasPrevious) {
+      setCurrentPage(loaderData?.prevPage);
     }
-  }, [searchResult.hasPrevious, searchResult.prevPage]);
+  };
 
-  const handleChangeLimit = useCallback((newLimit: SetStateAction<number>) => {
-    setLimit(newLimit);
-    setCurrentPage(1);
-  }, []);
+  const handleChangeLimit = useCallback(
+    (newLimit: SetStateAction<number>) => {
+      setLimit(newLimit);
+      setCurrentPage(1);
+      fetcher.load(`/search?page=1&limit=${newLimit}`);
+    },
+    [fetcher],
+  );
 
   return (
     <div className="space-y-6">
@@ -100,7 +106,7 @@ export default function SearchPage() {
         </div>
         <div className="flex w-full justify-center px-5 pb-1">
           <TablePagination
-            totalPages={searchResult.totalPages}
+            totalPages={loaderData?.totalPages}
             currentPage={currentPage}
             handlePrevious={handlePrevious}
             handleNext={(p) => handleNext(p as number)}
